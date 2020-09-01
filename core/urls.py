62b219c034b6085f79c633b3df63ae2d0bbf02c8
ronnil <ronniloreo@gmail.com>
2020-09-02 00:15:32 +0530

    install theme

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
-from .views import item_list
+from .views import item_list,checkout,product
 
 app_name = 'core'
 
 urlpatterns = [
-    path('/sbt', item_list, name= 'item_list')
+    path('', item_list, name= 'item_list'),
+    path('checkout/', checkout, name= 'checkout'),
+    path('product/', product, name= 'product')
 ]
